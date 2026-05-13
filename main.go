f445311561835fcc6feb633ef1d9530a80d6c262
sheetung <sheetung@users.noreply.github.com>
2026-05-13 15:17:56 +0800

    chore: bump version to v0.7.9

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ const (
 	webAddress = "127.0.0.1:47831"
 	// CurrentVersion is the application version
 	// This is compared with the latest GitHub release to check for updates
-	CurrentVersion = "v0.7.8"
+	CurrentVersion = "v0.7.9"
 )
 
 func main() {
